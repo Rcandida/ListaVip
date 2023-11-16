4e18f26c2e6a134017aa0886a790b6b46ba1e510
Rosa Vasconcelos <rcandidacursos@yahoo.com>
2023-11-16 10:31:06 -0300

    criacao do banco de dados

---
+++ app/src/main/java/br/edu/unichristus/listavip/database/ListaDB.java
@@ -0,0 +1,41 @@
+package br.edu.unichristus.listavip.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ListaDB extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "lista.db";
+    public static final int DB_VERSION = 1;
+
+    Cursor cursor;
+    SQLiteDatabase db;
+
+
+    public ListaDB(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+        db = getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sqlTabelaLista = "CREATE TABLE Lista (id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                "nome TEXT, " +
+                "matricula TEXT," +
+                "curso TEXT," +
+                "telefone TEXT)";
+
+        db.execSQL(sqlTabelaLista);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
